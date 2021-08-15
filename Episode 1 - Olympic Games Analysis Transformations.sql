068f5ee45d549ce0c6db3b4ce472c4513e0889f9
AnalyzeWithAli <75861401+AnalyzeWithAli@users.noreply.github.com>
2021-08-15 11:42:15 +0200

    Add files via upload

---
+++ Episode 1 - Olympic Games Analysis Transformations.sql
@@ -0,0 +1,24 @@
+SELECT
+         [ID]
+        ,[Name] AS 'Competitor Name' -- Renamed Column
+        ,CASE WHEN SEX = 'M' THEN 'Male' ELSE 'Female' END AS Sex -- Better name for filters and visualisations
+        ,[Age]
+		,CASE	WHEN [Age] < 18 THEN 'Under 18'
+				WHEN [Age] BETWEEN 18 AND 25 THEN '18-25'
+				WHEN [Age] BETWEEN 25 AND 30 THEN '25-30'
+				WHEN [Age] > 30 THEN 'Over 30'
+		 END AS [Age Grouping]
+        ,[Height]
+        ,[Weight]
+        ,[NOC] AS 'Nation Code' -- Explained abbreviation
+ --       ,CHARINDEX(' ', Games)-1 AS 'Example 1'
+ --       ,CHARINDEX(' ', REVERSE(Games))-1 AS 'Example 2'
+        ,LEFT(Games, CHARINDEX(' ', Games) - 1) AS 'Year' -- Split column to isolate Year, based on space
+--        ,RIGHT(Games,CHARINDEX(' ', REVERSE(Games))-1) AS 'Season' -- Split column to isolate Season, based on space
+--        ,[Games]
+--        ,[City] -- Commented out as it is not necessary for the analysis
+        ,[Sport]
+        ,[Event]
+        ,CASE WHEN Medal = 'NA' THEN 'Not Registered' ELSE Medal END AS Medal -- Replaced NA with Not Registered
+  FROM [olympic_games].[dbo].[athletes_event_results]
+  WHERE RIGHT(Games,CHARINDEX(' ', REVERSE(Games))-1) = 'Summer' -- Where Clause to isolate Summer Season
